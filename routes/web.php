52ca7fa3c6f4ddd307991c87b808857b62b39af0
Dave <d.enoc.494374@umindanao.edu.ph>
2026-04-27 19:30:05 +0800

    added a new file

--- routes/web.php
+++ routes/web.php
@@ -7,7 +7,7 @@ use App\Http\Controllers\AppointmentController;
 use App\Http\Controllers\PaymentController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
+Route::get('/home', function () {
     return view('welcome');
 });
 
